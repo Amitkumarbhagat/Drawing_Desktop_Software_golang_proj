697481c73145392a851cbf1e94ac5932aa3880ce
Amit Bhagat <78423707+Amitkumarbhagat@users.noreply.github.com>
2023-07-19 00:35:21 +0530

    added swatch.go

--- swatch/swatchrenderer.go
+++ swatch/swatchrenderer.go
@@ -1,1 +1,12 @@
 package swatch
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/canvas"
+)
+
+type SwatchRenderer struct {
+	square  canvas.Rectangle
+	objects []fyne.CanvasObject
+	parent  *Swatch
+}
